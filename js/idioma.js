7cf23d4e08c0db57677dfdb7ae05fae35a110b98
Fernando Gambino <fernando.m.gambino@gmail.com>
2022-10-31 12:01:03 -0300

    Se añade selector idioma es/en

---
+++ js/idioma.js
@@ -0,0 +1,11 @@
+var check=document.querySelector(".check");
+check.addEventListener('click, idioma');
+
+function idioma{
+    let id=check.ckecked;
+    if(id==true){
+        location.href="language/en.html";
+        } else{
+            location.href="../index.html";
+        }
+}
