2ae39f51d36c06993b2d145d51b790a4ee3f75be
Fernando Gambino <fernando.m.gambino@gmail.com>
2022-10-31 12:13:14 -0300

    Se actualiza idioma.js

--- js/idioma.js
+++ js/idioma.js
@@ -1,7 +1,7 @@
 var check=document.querySelector(".check");
 check.addEventListener('click, idioma');
 
-function idioma{
+function idioma(){
     let id=check.ckecked;
     if(id==true){
         location.href="language/en.html";
