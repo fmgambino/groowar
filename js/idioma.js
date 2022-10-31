69b94dd496796a60f735b6523f6aace8cc717606
Fernando Gambino <fernando.m.gambino@gmail.com>
2022-10-31 12:34:04 -0300

    Se realizan correcciones

--- js/idioma.js
+++ js/idioma.js
@@ -4,7 +4,7 @@ check.addEventListener('click', idioma);
 function idioma(){
     let id=check.ckecked;
     if(id==true){
-        location.href="en/index.html";
+        location.href="./en/index.html";
     }else{
             
         location.href="../index.html";
