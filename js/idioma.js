1996db91dd7ff2fde43d431fb2c911d84de8c840
Fernando Gambino <fernando.m.gambino@gmail.com>
2022-10-31 13:14:44 -0300

    Se aplican correcciones en idioma.js

--- js/idioma.js
+++ js/idioma.js
@@ -3,15 +3,11 @@ check.addEventListener('click', idioma);
 
 function idioma(){
     let id=check.ckecked;
-    console.log(check.checked);
-
+    
     if(id==true){
-        location.href="en/index.html";
-        console.log("se va a ingles");
-    }else{
-            
-        location.href="index.html";
-        console.log("Se va a espanish");
+        location.href="en/index.html";     
+    }else{            
+        location.href="../index.html";      
     }
     
 }
