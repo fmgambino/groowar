eabc295e1c15cee8f2dea247aa42329613e1d766
Fernando Gambino <fernando.m.gambino@gmail.com>
2022-10-31 17:24:47 -0300

    Se corrige linea 5 idioma.js

--- js/idioma.js
+++ js/idioma.js
@@ -2,7 +2,7 @@ var check=document.querySelector(".check");
 check.addEventListener('click', idioma);
 
 function idioma(){
-    let id=check.ckecked;
+    let id=check.checked;
     
     if(id==true){
         location.href="en/index.html";     
